6bf8dbec1045c1e73698b37bc84a918293e026c7
Jacob Walters <prog-github@jacobwalte.rs>
2024-09-29 14:54:44 +0100

    [add] Update geoJSON file to Esri World Countries Generalised

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ python3 countries.py
 ```
 
 Your svg file can be found in the current directory as `countries.svg`.
-If hosting this online, I recommend passing the output through an SVG optimiser to remove around 60% of the file size.
+If hosting this online, I recommend passing the output through an SVG optimiser to remove around 60% of the file size. [SVGOMG](https://svgomg.net/) works well for this.
 
 ## Acknowledgements
-The geoJSON file was generated from https://geojson-maps.kyd.au.
+The current geoJSON file is [Esri's World Countries Generalised](https://hub.arcgis.com/datasets/esri::world-countries-generalized) dataset. Previously, I used a build of Natural Earth's `countries-110m`, but this had many issues with borders (Crimea being displayed as russian territory instead of Ukrainian, French Guyana being displayed as part of France, etc). The new file is much more detailed, and has corrrect borders. You can adjust the scale (and resulting file size) in `countries.yaml`.
--- countries.py
+++ countries.py
@@ -3,32 +3,45 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 
+print('Loading configuration...')
 with open('countries.yaml', 'r') as file:
     data = yaml.safe_load(file)
 
 lived_countries = data.get('Lived', [])
 visited_countries = data.get('Visited', [])
+resolution = data.get('Resolution', 1000)
 
-hl  = data.get('Colours', {}).get('Lived', '#11f48e')
-hl2 = data.get('Colours', {}).get('Visited', '#b7f4d9')
-bg  = data.get('Colours', {}).get('Default', '#f4f4f4')
+lived_colour  = data.get('Colours', {}).get('Lived', '#001604')
+visited_colour = data.get('Colours', {}).get('Visited', '#00a871')
+default_colour  = data.get('Colours', {}).get('Default', '#f4f4f4')
+background_colour  = data.get('Colours', {}).get('Background', '#f5fffa')
 
-file = data.get('File', 'countries-110m.geojson')
-world = gpd.read_file(file).set_crs('ESRI:53001', allow_override=True).to_crs(epsg=3395)
+print('Loading borders...')
+file = data.get('File', 'countries.geojson')
+world = gpd.read_file(file).set_crs('EPSG:4326', allow_override=True).to_crs(epsg=3395)
 
-world = world[world['name'] != 'Antarctica']
-lived_geom = world[world['name'].isin(lived_countries)]
-visited_geom = world[world['name'].isin(visited_countries)]
+print('Simplifying map...')
+world["const"] = 0
+world = world[world["COUNTRY"] != 'Antarctica']
+#print(world.geometry)
+world_geom = world.dissolve(by = "const").geometry.simplify(resolution)
 
+lived_geom = world[world["COUNTRY"].isin(lived_countries)].dissolve(by = "const").geometry.simplify(resolution)
+visited_geom = world[world["COUNTRY"].isin(visited_countries)].dissolve(by = "const").geometry.simplify(resolution)
 
+print(world_geom)
+
+print('Generating output...')
 fig, ax = plt.subplots(figsize=(14, 6))
-world.plot(ax=ax, color=bg)
-visited_geom.plot(ax=ax, color=hl2, label='Visited', legend=True)
-lived_geom.plot(ax=ax, color=hl, label='Lived in', legend=True)
+world_geom.plot(ax=ax, color=default_colour)
+visited_geom.plot(ax=ax, color=visited_colour, label='Visited')
+lived_geom.plot(ax=ax, color=lived_colour, label='Lived in')
+fig.set_facecolor(background_colour)
 ax.set_axis_off()
 
-visited_patch = Patch(color=hl2, label='Visited')
-lived_patch = Patch(color=hl, label='Lived in')
+visited_patch = Patch(color=visited_colour, label='Visited')
+lived_patch = Patch(color=lived_colour, label='Lived in')
 plt.legend(handles=[visited_patch, lived_patch], loc='lower left', frameon=False)
 
 plt.savefig('countries.svg', format='svg', bbox_inches='tight')
+print('Done. See `countries.svg` for output.')
